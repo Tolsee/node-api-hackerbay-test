98e0cb0338453a0bb065061c4c8f77902d08b811
Tolsee <tolsee3@gmail.com>
2018-06-29 15:19:07 +0545

    Add logger

--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,39 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import fs from 'fs';
+import path from 'path';
+import rfs from 'rotating-file-stream';
+import morgan from 'morgan';
 import createController from "../utils/createController";
 
 import * as index from './controller';
 import * as data from './controller/data';
 
-const App = express();
+const app = express();
 const Router = express.Router();
 
-App.use(bodyParser.urlencoded({ extended: false }));
-App.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+const accessLog = path.join(__dirname, '../../logs');
+fs.existsSync(accessLog) || fs.mkdirSync(accessLog);
+const accessLogStream = rfs((time, index) => time ? `${time.getFullYear()}-${time.getMonth()}-${time.getDate()}-${index}.log` : `access-${index}.log`, {
+  interval: '1d',
+  path: accessLog
+});
+const logger = morgan('short', { stream: accessLogStream});
+
+app.use(logger);
+
+/*
+* ----------------
+* Router
+* Setup
+* ----------------
+* */
 createController('/', index, Router);
 createController('/data', data, Router);
 
-export default App.use(Router);
+app.use(Router);
+
+export default app;
